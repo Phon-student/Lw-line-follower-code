c786b425186373759769497c5f4f039ffc3a36a3
Tanakorn Youngmeesuk <phon.youngmeesuk@gmail.com>
2022-11-23 22:30:23 +0700

    [+][-] Update for IR testing
    [ ] still need to connect motors for testing

--- PID.ino
+++ PID.ino
@@ -2,25 +2,26 @@ int Motorspd = 0 , MotorInitial = 100;
 int kp = 1, ki = 0, kd = 0;
 int error = 0, lastError = 0, integral = 0, derivative = 0;
 
-int IRfarleft = 2, IRleft = 3,IRmid = 4, IRright = 5, IRfarright = 6;
+int IRfarleft = 0, IRleft = 2,IRmid = 4, IRright = 6, IRfarright = 8;
 
 
 void setup(){
   Serial.begin(9600);
   Serial.println("PID");
-  pinMod(IRfarleft, INPUT);
+  pinMode(IRfarleft, INPUT);
   pinMode(IRleft, INPUT);
   pinMode(IRmid, INPUT);
   pinMode(IRright, INPUT);
   pinMode(IRfarright, INPUT);
-
-  int sensor[5] = {IRfarleft, IRleft, IRmid, IRright, IRfarright};
 }
 
 void loop(){
-  while(true){
-    //print check sensor
-    Serial.print("farleft: ");
+  read();
+  
+} 
+
+void read(){
+  Serial.print("farleft: ");
     Serial.print(digitalRead(IRfarleft));
     Serial.print(" left: ");
     Serial.print(digitalRead(IRleft));
@@ -31,6 +32,5 @@ void loop(){
     Serial.print(" farright: ");
     Serial.println(digitalRead(IRfarright));
     delay(1000);
-  }
-} 
+}
 
