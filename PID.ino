95a660437747b8852ebdd0fce1d8351d68010f49
Tanakorn Youngmeesuk <phon.youngmeesuk@gmail.com>
2022-11-23 21:52:25 +0700

    [+] Update PID with new set of testing code

--- PID.ino
+++ PID.ino
@@ -1,9 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
+int Motorspd = 0 , MotorInitial = 100;
+int kp = 1, ki = 0, kd = 0;
+int error = 0, lastError = 0, integral = 0, derivative = 0;
 
+int IRfarleft = 2, IRleft = 3,IRmid = 4, IRright = 5, IRfarright = 6;
+
+
+void setup(){
+  Serial.begin(9600);
+  Serial.println("PID");
+  pinMod(IRfarleft, INPUT);
+  pinMode(IRleft, INPUT);
+  pinMode(IRmid, INPUT);
+  pinMode(IRright, INPUT);
+  pinMode(IRfarright, INPUT);
+
+  int sensor[5] = {IRfarleft, IRleft, IRmid, IRright, IRfarright};
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  while(true){
+    //print check sensor
+    Serial.print("farleft: ");
+    Serial.print(digitalRead(IRfarleft));
+    Serial.print(" left: ");
+    Serial.print(digitalRead(IRleft));
+    Serial.print(" mid: ");
+    Serial.print(digitalRead(IRmid));
+    Serial.print(" right: ");
+    Serial.print(digitalRead(IRright));
+    Serial.print(" farright: ");
+    Serial.println(digitalRead(IRfarright));
+    delay(1000);
+  }
+} 
 
-}
