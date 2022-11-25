13b2ca035b19ee999cd435ecfaa8a930376c1126
Tanakorn Youngmeesuk <phon.youngmeesuk@gmail.com>
2022-11-25 18:54:37 +0700

    [+] Update PID

--- PID.ino
+++ PID.ino
@@ -1,74 +1,58 @@
+#define motorPin1  3
+#define motorPin2  4
+#define IRfarleft = A1 
+#define IRleft = A2
+#define IRmid = A4
+#define IRright = A6
+#define IRfarright = A8
+
 int Motorspd = 0 , MotorInitial = 100;
 int kp = 1, ki = 0, kd = 0;
 int lastError = 0, derivative = 0;
 
 
-int IRfarleft = 0, IRleft = 2,IRmid = 4, IRright = 6, IRfarright = 8;
-
-
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(19200);
   Serial.println("PID");
   pinMode(IRfarleft, INPUT);
   pinMode(IRleft, INPUT);
   pinMode(IRmid, INPUT);
   pinMode(IRright, INPUT);
   pinMode(IRfarright, INPUT);
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
 }
 
 void loop(){
   read();
   PID();
   Serial.println(Motorspd);
-} 
-int error(){
-  sensor[0] << IRfarleft; 
-  sensor[1] << IRleft;
-  sensor[2] << IRmid;
-  sensor[3] << IRright;
-  sensor[4] << IRfarright;
 
-  if((sensor[0]==0)&&(sensor[1]==0)&&(sensor[2]==0)&&(sensor[3]==0)&&(sensor[4]==1))
-  error=4;
-  else if((sensor[0]==0)&&(sensor[1]==0)&&(sensor[2]==0)&&(sensor[3]==1)&&(sensor[4]==1))
-  error=3;
-  else if((sensor[0]==0)&&(sensor[1]==0)&&(sensor[2]==0)&&(sensor[3]==1)&&(sensor[4]==0))
-  error=2;
-  else if((sensor[0]==0)&&(sensor[1]==0)&&(sensor[2]==1)&&(sensor[3]==1)&&(sensor[4]==0))
-  error=1;
-  else if((sensor[0]==0)&&(sensor[1]==0)&&(sensor[2]==1)&&(sensor[3]==0)&&(sensor[4]==0))
-  error=0;
-  else if((sensor[0]==0)&&(sensor[1]==1)&&(sensor[2]==1)&&(sensor[3]==0)&&(sensor[4]==0))
-  error=-1;
-  else if((sensor[0]==0)&&(sensor[1]==1)&&(sensor[2]==0)&&(sensor[3]==0)&&(sensor[4]==0))
-  error=-2;
-  else if((sensor[0]==1)&&(sensor[1]==1)&&(sensor[2]==0)&&(sensor[3]==0)&&(sensor[4]==0))
-  error=-3;
-  else if((sensor[0]==1)&&(sensor[1]==0)&&(sensor[2]==0)&&(sensor[3]==0)&&(sensor[4]==0))
-  error=-4;
-  else if((sensor[0]==0)&&(sensor[1]==0)&&(sensor[2]==0)&&(sensor[3]==0)&&(sensor[4]==0))
-    if(error==-4) error=-5;
-    else error=5;
-  return error;
-}
+} 
+
 
 void read(){
-    Serial.print("farleft: ");
-    Serial.print(digitalRead(IRfarleft));
-    Serial.print(" left: ");
-    Serial.print(digitalRead(IRleft));
-    Serial.print(" mid: ");
-    Serial.print(digitalRead(IRmid));
-    Serial.print(" right: ");
-    Serial.print(digitalRead(IRright));
-    Serial.print(" farright: ");
-    Serial.println(digitalRead(IRfarright));
+    Serial.println(analogRead(IRfarleft));
+    Serial.println(analogRead(IRleft));
+    Serial.println(analogRead(IRmid));
+    Serial.println(analogRead(IRright));
+    Serial.println(analogRead(IRfarright));
+  
     delay(1000);
 }
 
-void PID(int error){
+void PID(){
+  int error = analogRead(IRmid) - 500;
   derivative = error - lastError;
-  Motorspd = MotorInitial + (kp*error) + (kd*derivative);
+  Motorspd = MotorInitial + (kp * error) + (ki * error) + (kd * derivative);
   lastError = error;
-  Motorspd = MotorInitial;
+  if(Motorspd > 255){
+    Motorspd = 255;
+  }
+  if(Motorspd < 0){
+    Motorspd = 0;
+  }
+  analogWrite(motorPin1, Motorspd);
+  analogWrite(motorPin2, Motorspd);
 }
+
